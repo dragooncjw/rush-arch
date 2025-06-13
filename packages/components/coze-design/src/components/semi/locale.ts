10f6b0c6420dc545ea3354110a6b0aed05a82472
chenjiawei.inizio <289056872@qq.com>
2025-06-13 11:26:18 +0800

    fix: locale (#60)

--- packages/components/coze-design/src/components/semi/locale.ts
+++ packages/components/coze-design/src/components/semi/locale.ts
@@ -1,5 +1,5 @@
 //  Copyright (c) 2025 coze-dev
 //  SPDX-License-Identifier: MIT
 
-export { default as zhCN } from '@douyinfe/semi-ui/lib/es/locale/source/zh_CN';
-export { default as enUS } from '@douyinfe/semi-ui/lib/es/locale/source/en_US';
+export { default as zhCN } from '@douyinfe/semi-ui/lib/es/locale/source/zh_CN.js';
+export { default as enUS } from '@douyinfe/semi-ui/lib/es/locale/source/en_US.js';
