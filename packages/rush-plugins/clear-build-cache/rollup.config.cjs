43622344ddfd0762994c5f39d1a76ee3ef74a6ff
tecvan <84165678+Tecvan-fe@users.noreply.github.com>
2025-06-17 16:10:32 +0800

    chore: setup publish config for @coze-arch/rush-clear-build-cache-plugin (#73)
    Co-authored-by: Tecvan-fe <tecvan.fe@qq.com>

---
+++ packages/rush-plugins/clear-build-cache/rollup.config.cjs
@@ -0,0 +1,15 @@
+//  Copyright (c) 2025 coze-dev
+//  SPDX-License-Identifier: MIT
+require('sucrase/register/ts');
+const { defineConfig } = require('@coze-arch/build-lib-preset');
+
+module.exports = defineConfig({
+  rootDir: process.cwd(),
+  output: [
+    {
+      file: 'lib/index.js',
+      format: 'cjs',
+      sourcemap: false,
+    },
+  ],
+});
