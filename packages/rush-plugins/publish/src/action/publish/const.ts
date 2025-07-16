27c1623f40850f0ee05b6ee530dde882fa588ef3
tecvan <84165678+Tecvan-fe@users.noreply.github.com>
2025-07-16 17:48:37 +0800

    fix: common usage for private git registry (#86)
    Co-authored-by: Tecvan-fe <tecvan.fe@qq.com>

--- packages/rush-plugins/publish/src/action/publish/const.ts
+++ packages/rush-plugins/publish/src/action/publish/const.ts
@@ -1,4 +1,4 @@
 //  Copyright (c) 2025 coze-dev
 //  SPDX-License-Identifier: MIT
 
-export const GIT_REPO_URL_REGEX = /git@github\.com:([^\/]+)\/([^\.]+)\.git/;
+export const GIT_REPO_URL_REGEX = /git@.+:([^\/]+)\/([^\.]+)\.git/;
