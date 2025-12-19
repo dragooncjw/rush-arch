c649e51ff3264f74561213f31d3762d60c3c59d1
stream-pipe <215027396+stream-pipe@users.noreply.github.com>
2025-12-19 16:47:30 +0800

    fix: remove value option (#122)
    Co-authored-by: stream-pipe <stream-pipe@users.noreply.github.com>

--- packages/text-editor/preset-code-languages/src/preset.ts
+++ packages/text-editor/preset-code-languages/src/preset.ts
@@ -1,10 +1,7 @@
-import { value, valueExtension } from '@coze-editor/core-plugins';
-import { extension, option, asyncOption } from '@coze-editor/core';
+import { asyncOption } from '@coze-editor/core';
 
 import { getLanguage } from './languages';
 
 export const preset = [
-  extension(valueExtension),
-  option('value', value),
   ...asyncOption('path', async (path: string) => getLanguage(path)),
 ];
